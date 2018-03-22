8c9c985a54350a55e761bff2c46c06fd2c1ae17c
baohuaw <baohuaw@amazon.com>
2018-03-22 17:04:13 +0800

    minor update

---
+++ seq2seq.py
@@ -0,0 +1,157 @@
+import tensorflow as tf
+import numpy as np
+import random
+import math
+from matplotlib import pyplot as plt
+import os
+import copy
+
+x = np.linspace(0, 30, 105)
+y = 2 * np.sin(x)
+
+l1, = plt.plot(x[:85], y[:85], 'y', label = 'training samples')
+l2, = plt.plot(x[85:], y[85:105], 'c--', label = 'test samples')
+plt.legend(handles = [l1, l2], loc = 'upper left')
+plt.show()
+#
+# train_y = y.copy()
+#
+# noise_factor = 0.5
+# train_y += np.random.randn(105) * noise_factor
+#
+# l1, = plt.plot(x[:85], train_y[:85], 'yo', label = 'training samples')
+# plt.plot(x[:85], y[:85], 'y:')
+# l2, = plt.plot(x[85:], train_y[85:], 'co', label = 'test samples')
+# plt.plot(x[85:], y[85:], 'c:')
+# plt.legend(handles = [l1, l2], loc = 'upper left')
+# plt.show()
+
+input_seq_len = 15
+output_seq_len = 20
+
+x = np.linspace(0, 30, 105)
+train_data_x = x[:85]
+
+
+def true_signal(x):
+    y = 2 * np.sin(x)
+    return y
+
+
+def noise_func(x, noise_factor=1):
+    return np.random.randn(len(x)) * noise_factor
+
+
+def generate_y_values(x):
+    return true_signal(x) + noise_func(x)
+
+
+def generate_train_samples(x=train_data_x, batch_size=10, input_seq_len=input_seq_len, output_seq_len=output_seq_len):
+    total_start_points = len(x) - input_seq_len - output_seq_len
+    start_x_idx = np.random.choice(range(total_start_points), batch_size)
+
+    input_seq_x = [x[i:(i + input_seq_len)] for i in start_x_idx]
+    output_seq_x = [x[(i + input_seq_len):(i + input_seq_len + output_seq_len)] for i in start_x_idx]
+
+    input_seq_y = [generate_y_values(x) for x in input_seq_x]
+    output_seq_y = [generate_y_values(x) for x in output_seq_x]
+
+    # batch_x = np.array([[true_signal()]])
+    return np.array(input_seq_y), np.array(output_seq_y)
+
+
+input_seq, output_seq = generate_train_samples(batch_size=10)
+
+l1, = plt.plot(range(15), input_seq[1], 'go', label='input sequence for one sample')
+l2, = plt.plot(range(15, 35), output_seq[1], 'ro', label='output sequence for one sample')
+plt.legend(handles=[l1, l2], loc='lower left')
+plt.show()
+
+results = []
+for i in range(100):
+    temp = generate_y_values(x)
+    results.append(temp)
+results = np.array(results)
+
+for i in range(100):
+    l1, = plt.plot(results[i].reshape(105, -1), 'co', lw=0.1, alpha=0.05, label='noisy training data')
+
+l2, = plt.plot(true_signal(x), 'm', label='hidden true signal')
+plt.legend(handles=[l1, l2], loc='lower left')
+plt.show()
+
+###############################################
+from build_model_basic import *
+
+## Parameters
+learning_rate = 0.01
+lambda_l2_reg = 0.003
+
+## Network Parameters
+# length of input signals
+input_seq_len = 15
+# length of output signals
+output_seq_len = 20
+# size of LSTM Cell
+hidden_dim = 64
+# num of input signals
+input_dim = 1
+# num of output signals
+output_dim = 1
+# num of stacked lstm layers
+num_stacked_layers = 2
+# gradient clipping - to avoid gradient exploding
+GRADIENT_CLIPPING = 2.5
+
+total_iteractions = 100
+batch_size = 16
+KEEP_RATE = 0.5
+train_losses = []
+val_losses = []
+
+x = np.linspace(0, 30, 105)
+train_data_x = x[:85]
+
+rnn_model = build_graph(feed_previous=False)
+
+saver = tf.train.Saver()
+
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+
+    for i in range(total_iteractions):
+        batch_input, batch_output = generate_train_samples(batch_size=batch_size)
+
+        feed_dict = {rnn_model['enc_inp'][t]: batch_input[:, t].reshape(-1, input_dim) for t in range(input_seq_len)}
+        feed_dict.update(
+            {rnn_model['target_seq'][t]: batch_output[:, t].reshape(-1, output_dim) for t in range(output_seq_len)})
+        _, loss_t = sess.run([rnn_model['train_op'], rnn_model['loss']], feed_dict)
+        print(loss_t)
+
+    temp_saver = rnn_model['saver']()
+    save_path = temp_saver.save(sess, os.path.join('./', 'univariate_ts_model0'))
+
+print("Checkpoint saved at: ", save_path)
+
+test_seq_input = true_signal(train_data_x[-15:])
+
+rnn_model = build_graph(feed_previous=True)
+
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+
+    saver = rnn_model['saver']().restore(sess, os.path.join('./', 'univariate_ts_model0'))
+
+    feed_dict = {rnn_model['enc_inp'][t]: test_seq_input[t].reshape(1, 1) for t in range(input_seq_len)}
+    feed_dict.update({rnn_model['target_seq'][t]: np.zeros([1, output_dim]) for t in range(output_seq_len)})
+    final_preds = sess.run(rnn_model['reshaped_outputs'], feed_dict)
+
+    final_preds = np.concatenate(final_preds, axis=1)
+
+l1, = plt.plot(range(85), true_signal(train_data_x[:85]), label = 'Training truth')
+l2, = plt.plot(range(85, 105), y[85:], 'yo', label = 'Test truth')
+l3, = plt.plot(range(85, 105), final_preds.reshape(-1), 'ro', label = 'Test predictions')
+plt.legend(handles = [l1, l2, l3], loc = 'lower left')
+plt.show()
